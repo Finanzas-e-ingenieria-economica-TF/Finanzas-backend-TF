24b2bd4bbb2a2e884e4ed97818474d49c4f835a8
JoseRiega <u202211254@upc.edu.pe>
2025-07-09 12:50:59 -0500

    fix: changes for entity founder.

--- src/main/java/com/example/finanzas_back/FinanzasBackApplication.java
+++ src/main/java/com/example/finanzas_back/FinanzasBackApplication.java
@@ -2,8 +2,10 @@ package com.example.finanzas_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.example.finanzas_back.model")
 public class FinanzasBackApplication {
 
     public static void main(String[] args) {
